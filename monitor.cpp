dff1dfa79837dc9cef99a563a48372dcc0675542
Shiv Sharma <shivsharma@adobe.com>
2025-05-30 08:01:34 +0530

    finxing linting

--- monitor.cpp
+++ monitor.cpp
@@ -6,40 +6,38 @@
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 struct VitalThreshold {
-    float min;
-    float max;
-    const char* name;
-    const char* alertMessage;
+  float min;
+  float max;
+  const char* alertMessage;
 };
 
-bool isVitalInRange(float value, VitalThreshold threshold) {
-    return value >= threshold.min && value <= threshold.max;
+bool isVitalInRange(float value, const VitalThreshold& threshold) {
+  return value >= threshold.min && value <= threshold.max;
 }
 
 void showAlert(const char* message) {
-    cout << message << "\n";
-    for (int i = 0; i < 6; i++) {
-        cout << "\r* " << flush;
-        sleep_for(seconds(1));
-        cout << "\r *" << flush;
-        sleep_for(seconds(1));
-    }
-}
-
-bool checkAndAlert(float value, VitalThreshold threshold) {
-    if (!isVitalInRange(value, threshold)) {
-        showAlert(threshold.alertMessage);
-        return false;
-    }
-    return true;
+  cout << message << "\n";
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-    VitalThreshold tempThresh = {95, 102, "Temperature", "Temperature is critical!"};
-    VitalThreshold pulseThresh = {60, 100, "Pulse Rate", "Pulse Rate is out of range!"};
-    VitalThreshold spo2Thresh = {90, 100, "SPO2", "Oxygen Saturation out of range!"};
+  const VitalThreshold thresholds[] = {
+    {95, 102, "Temperature is critical!"},
+    {60, 100, "Pulse Rate is out of range!"},
+    {90, 100, "Oxygen Saturation out of range!"}
+  };
+  const float values[] = {temperature, pulseRate, spo2};
 
-    return checkAndAlert(temperature, tempThresh)
-        && checkAndAlert(pulseRate, pulseThresh)
-        && checkAndAlert(spo2, spo2Thresh);
+  for (int i = 0; i < 3; ++i) {
+    if (!isVitalInRange(values[i], thresholds[i])) {
+      showAlert(thresholds[i].alertMessage);
+      return 0;
+    }
+  }
+  return 1;
 }
